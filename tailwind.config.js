ff6f17485a06a58324e7ae396535558f1f0598ff
Smruti Somyak Pradhan <92969636+Tick2003@users.noreply.github.com>
2025-05-31 11:11:24 +0530

    Enhance dark mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
   theme: {
     extend: {
       colors: {
+        dark: {
+          50: '#18181B',
+          100: '#27272A',
+          200: '#3F3F46',
+          300: '#52525B',
+          400: '#71717A',
+          500: '#A1A1AA',
+          600: '#D4D4D8',
+          700: '#E4E4E7',
+          800: '#F4F4F5',
+          900: '#FAFAFA',
+        },
         primary: {
           50: '#eef2ff',
           100: '#e0e7ff',
